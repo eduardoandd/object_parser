6b676cb573b2c3eee8a7eb7f5de34082992fb28e
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-13 20:50:36 -0300

    tirando screenshot da tela e fazendo requisição

--- lib/pages/camera_with_voice.dart
+++ lib/pages/camera_with_voice.dart
@@ -4,10 +4,15 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:picovoice_flutter/picovoice_error.dart';
 import 'package:picovoice_flutter/picovoice_manager.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:rhino_flutter/rhino.dart';
+import 'package:http/http.dart' as http;
+
 
 class CameraWithVoiceControl extends StatefulWidget {
   final List<CameraDescription> cameras;
@@ -28,10 +33,89 @@ class _CameraWithVoiceControlState extends State<CameraWithVoiceControl> {
   String keywordPath = "assets/Glauco_pt_android_v3_0_0.ppn";
   String contextPath = "assets/Magic-Camera_pt_android_v3_0_0.rhn"; 
   String porcupineModelPath = "assets/porcupine_params_pt.pv"; 
-  String rhinoModelPath = "assets/rhino_params_pt.pv"; 
+  String rhinoModelPath = "assets/rhino_params_pt.pv";
+  FlutterSoundRecorder? _audioRecorder; 
+  bool _isRecording = false;
+  final ScreenshotController _screenshotController = ScreenshotController();
+  File? file;
+  bool isUploading = false;
 
-  
+
+
   PicovoiceManager? _picovoiceManager;
+
+  Future<void> _screenShotPage() async {
+   print("Wake word detected!");
+  
+  try {
+    // Capture the screenshot
+    final image = await _screenshotController.capture();
+    if (image != null) {
+      // Get the directory to save the file
+      final directory = await getExternalStorageDirectory(); // Para Android
+      // final directory = await getApplicationDocumentsDirectory(); // Para iOS
+  
+      if (directory != null) {
+        final filePath = '${directory.path}/screenshot_${DateTime.now().millisecondsSinceEpoch}.png';
+        file = File(filePath);
+        setState(() {});
+        await file!.writeAsBytes(image);
+        print("Screenshot salva em: $filePath");
+      } else {
+        print("Não foi possível acessar o diretório.");
+      }
+    } else {
+      print("Falha ao capturar a screenshot.");
+    }
+  } catch (e) {
+    print("Erro ao salvar a screenshot: $e");
+  }
+}
+
+Future<void> uploadAudioAndImage(File file) async{
+    if(file == null){
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("imagem ou audio não reconhecidos!"))
+      );
+      return;
+    }
+   
+    setState(() {
+      isUploading = true;
+    });
+
+    try {
+      final uri = Uri.parse('http://192.168.0.220:8000/object_img/');
+      var request = http.MultipartRequest('POST', uri)
+        ..files.add(await http.MultipartFile.fromPath('image',file.path));
+        // ..files.add(await http.MultipartFile.fromPath('audio', recordedFilePath));
+
+        final response = await request.send();
+        if(response.statusCode == 201){
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Upload realizado com sucesso!")),
+          );
+        }
+        else{
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Erro no upload: ${response.statusCode}")),
+          );
+        }
+      
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Erro ao enviar: $e")),
+      );
+      print(e);
+    }
+    finally {
+      setState(() {
+        isUploading = false;
+      });
+    }
+  }
+
+
 
   void _initPicovoice() async {
   try {
@@ -44,8 +128,7 @@ class _CameraWithVoiceControlState extends State<CameraWithVoiceControl> {
         porcupineModelPath: porcupineModelPath,
         rhinoModelPath: rhinoModelPath
       );
-
-
+      _audioRecorder = FlutterSoundRecorder();
 
     // start audio processing
     _picovoiceManager!.start();        
@@ -54,19 +137,13 @@ class _CameraWithVoiceControlState extends State<CameraWithVoiceControl> {
   }
 }
 
-void _wakeWordCallback() {
-  print("wake word detected!");
+Future<void> _wakeWordCallback() async  {
+    await _screenShotPage();
+    await uploadAudioAndImage(file!);
 }
 
 void _inferenceCallback(RhinoInference inference) {
-  if (inference.isUnderstood == true){
-    print("Comando reconhecido: ${inference.intent}");
-     print("Slots: ${inference.slots}");
-
-  }
-  else{
-    print("Comando não reconhecido.");
-  }
+  print(inference);
   
 }
   
@@ -120,63 +197,66 @@ Widget build(BuildContext context) {
     return Center(child: CircularProgressIndicator());
   }
 
-  return Scaffold(
-    appBar: AppBar(
-      title: Center(child: Text('Magic Camera')),
-    ),
-    body: Stack(
-      children: [
-        Positioned.fill(
-          child: GestureDetector(
-            onTapDown: (TapDownDetails details) async {
-              final RenderBox box = context.findRenderObject() as RenderBox;
-              final Offset localPosition = box.globalToLocal(details.globalPosition);
-              final Size size = box.size;
-
-              // Calcula coordenadas normalizadas
-              final double x = localPosition.dx / size.width;
-              final double y = localPosition.dy / size.height;
-
-              try {
-                await cameraController.setFocusPoint(Offset(x, y));
-              } catch (e) {
-                print('Erro ao definir ponto de foco: $e');
-              }
-            },
-            child: RotatedBox(
-              quarterTurns: cameraController.description.lensDirection == CameraLensDirection.back ? 1 : 3,
-              child: CameraPreview(cameraController),
+  return Screenshot(
+    controller: _screenshotController,
+    child: Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Magic Camera')),
+      ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: GestureDetector(
+              onTapDown: (TapDownDetails details) async {
+                final RenderBox box = context.findRenderObject() as RenderBox;
+                final Offset localPosition = box.globalToLocal(details.globalPosition);
+                final Size size = box.size;
+    
+                // Calcula coordenadas normalizadas
+                final double x = localPosition.dx / size.width;
+                final double y = localPosition.dy / size.height;
+    
+                try {
+                  await cameraController.setFocusPoint(Offset(x, y));
+                } catch (e) {
+                  print('Erro ao definir ponto de foco: $e');
+                }
+              },
+              child: RotatedBox(
+                quarterTurns: cameraController.description.lensDirection == CameraLensDirection.back ? 1 : 3,
+                child: CameraPreview(cameraController),
+              ),
             ),
           ),
-        ),
-        Positioned(
-          bottom: 20,
-          left: 20,
-          right: 20,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  if (cameraController.description.lensDirection == CameraLensDirection.back) {
-                    cameraController = CameraController(frontCamera, ResolutionPreset.high);
-                  } else {
-                    cameraController = CameraController(backCamera, ResolutionPreset.high);
-                  }
-
-                  await cameraController.initialize();
-                  setState(() {});
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  padding: EdgeInsets.all(16),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    if (cameraController.description.lensDirection == CameraLensDirection.back) {
+                      cameraController = CameraController(frontCamera, ResolutionPreset.high);
+                    } else {
+                      cameraController = CameraController(backCamera, ResolutionPreset.high);
+                    }
+    
+                    await cameraController.initialize();
+                    setState(() {});
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(16),
+                  ),
+                  child: Icon(Icons.flip_camera_android, color: Colors.white),
                 ),
-                child: Icon(Icons.flip_camera_android, color: Colors.white),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import audioplayers_darwin
 import file_selector_macos
 import just_audio
 import path_provider_foundation
+import record_darwin
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
